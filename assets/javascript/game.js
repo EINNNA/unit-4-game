c4336c3dcf6f395e6938996e173d55d85adffd97
EINNNA <annie_t_@hotmail.com>
2019-08-19 23:41:48 +1000

    added base index, and some jquery and images

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    var blueGem = "";
+    var diamondGem = "";
+    var greenGem = "";
+    var redGem = "";
+    
+    function addition() {
+        $(".blueGem").on("click", function(){
+            console.log("hi");
+          });
+        $(".diamondGem").on("click", function() {
+            alert("Hi");
+        });
+        $(".greenGem").on("click", function() {
+            alert("Hi");
+        });
+        $(".redGem").on("click", function() {
+            alert("Hi");
+        });
+
+    });
+
+});
+
+
+
