d42a041e0836c5c6c044abeb0c1cac08864d16ff
EINNNA <annie_t_@hotmail.com>
2019-08-20 23:13:54 +1000

    added JS and edited HTML

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,63 @@
-$(document).ready(function() {
-    var blueGem = "";
-    var diamondGem = "";
-    var greenGem = "";
-    var redGem = "";
-    
+//global variables 
+var numGuess = Math.floor(Math.random() * 200);
+    console.log(numGuess);
+
+var goalNumber = numGuess;
+    console.log(goalNumber);
+
+var addedNumbers = 0;
+var wins = 0;
+var losses = 0;
+//var gameTrue = true;
+
+// functions
+$(document).ready(function () {
+    //if (game == true) {
+        //gem clicks
+        var blueGem = 4;
+        var diamondGem = 9;
+        var greenGem = 6;
+        var redGem = 2;
+        //text
+        $(".total").text(addedNumbers);
+        $(".winNumber").text(wins);
+        $(".loseNumber").text(losses);
+        $(".randomNumber").text(goalNumber);
 
         $(".blueGem").on("click", function() {
-            console.log("hi");
+            addedNumbers = blueGem + addedNumbers;
+            console.log(addedNumbers)
+            $(".total").text(addedNumbers);
+            winLose();
           });
         $(".diamondGem").on("click", function() {
-            alert("Hi");
+            addedNumbers = diamondGem + addedNumbers;
+            console.log(addedNumbers)
+            $(".total").text(addedNumbers);
+            winLose();
         });
         $(".greenGem").on("click", function() {
-            alert("Hi");
+            addedNumbers = diamondGem + addedNumbers;
+            console.log(addedNumbers)
+            $(".total").text(addedNumbers);
+            winLose();
         });
         $(".redGem").on("click", function() {
-            alert("Hi");
+            addedNumbers = blueGem + addedNumbers;
+            console.log(addedNumbers)
+            $(".total").text(addedNumbers);
+            winLose();
         });
-
+//winlosecon    
+        function winLose() {
+        if (goalNumber == addedNumbers) {
+            $(".instructions").text("YOU WIN!");
+        }
+        else if (goalNumber < addedNumbers) {
+            $(".instructions").text("YOU LOSE!");
+            game = false;
+        }};
+    //}
+        
 
 });
