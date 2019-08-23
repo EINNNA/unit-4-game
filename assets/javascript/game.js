714bf584358ba1600fbeea3dac67649cb04c5d92
EINNNA <annie_t_@hotmail.com>
2019-08-23 19:12:38 +1000

    fixing code, made crystal numbers random

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,13 @@
 //global variables 
-var numGuess = Math.floor(Math.random() * 200);
+//var maxCryst = 12;
+//var minCryst = 1;
+var max = 120;
+var min = 19;
+
+
+var numGuess = Math.floor(Math.random() * (max - min + 1));
     console.log(numGuess);
+
 
 var goalNumber = numGuess;
     console.log(goalNumber);
@@ -8,56 +15,91 @@ var goalNumber = numGuess;
 var addedNumbers = 0;
 var wins = 0;
 var losses = 0;
-//var gameTrue = true;
+//var game = true;
 
 // functions
 $(document).ready(function () {
-    //if (game == true) {
-        //gem clicks
-        var blueGem = 4;
-        var diamondGem = 9;
-        var greenGem = 6;
-        var redGem = 2;
+
+    var gemRandom = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+    var blueGem = gemRandom[Math.floor(Math.random() * gemRandom.length)];   
+    var diamondGem = gemRandom[Math.floor(Math.random() * gemRandom.length)];
+    var greenGem = gemRandom[Math.floor(Math.random() * gemRandom.length)];
+    var redGem = gemRandom[Math.floor(Math.random() * gemRandom.length)];
+
+    console.log(blueGem);
+    console.log(diamondGem);
+    console.log(greenGem);
+    console.log(redGem);
+
+
         //text
+        $(".instructions").text("GET THE SAME AMOUNT OF CRYSTALS.");
         $(".total").text(addedNumbers);
         $(".winNumber").text(wins);
         $(".loseNumber").text(losses);
         $(".randomNumber").text(goalNumber);
 
+        //gem adding
         $(".blueGem").on("click", function() {
             addedNumbers = blueGem + addedNumbers;
             console.log(addedNumbers)
             $(".total").text(addedNumbers);
             winLose();
           });
+
         $(".diamondGem").on("click", function() {
             addedNumbers = diamondGem + addedNumbers;
             console.log(addedNumbers)
             $(".total").text(addedNumbers);
             winLose();
         });
+
+
         $(".greenGem").on("click", function() {
             addedNumbers = diamondGem + addedNumbers;
             console.log(addedNumbers)
             $(".total").text(addedNumbers);
             winLose();
         });
+
+
         $(".redGem").on("click", function() {
             addedNumbers = blueGem + addedNumbers;
             console.log(addedNumbers)
             $(".total").text(addedNumbers);
             winLose();
         });
+
+
+
+
 //winlosecon    
         function winLose() {
-        if (goalNumber == addedNumbers) {
+        if (addedNumbers == goalNumber) {
             $(".instructions").text("YOU WIN!");
+            //$(".randomNumber", ".total", ".instructions").empty();
+            wins++;
+            winNumber = 0;
+            loseNumber = 0;
+            total = 0;
+            goalNumber = numGuess;
         }
         else if (goalNumber < addedNumbers) {
             $(".instructions").text("YOU LOSE!");
-            game = false;
+            losses--;
+            winNumber = 0;
+            loseNumber = 0;
+            total = 0;
+            goalNumber = numGuess;
         }};
-    //}
-        
 
-});
+        /*$(".hide").on("click", function(){
+            $("div").removeClass(".hide");
+            $(".winNumber, .loseNumber, .total, randomNumber").empty();
+            winNumber = 0;
+            loseNumber = 0;
+            total = 0;
+            randomNumber = goalNumber;
+        });*/       
+});   
